101bae3dbb532f504a621cf0e8eae361e1acaef4
ashwithans <ashwithans@gmail.com>
2024-01-15 15:25:23 +0530

    Phase4PracProj4Docker

---
+++ PracProj/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PracProj.Models;
+using System.Diagnostics;
+
+namespace PracProj.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+
+        List<User> userlist = new List<User>();
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+
+            userlist.Add(new Models.User { Username = "Siva", Password = "siva@123" });
+            userlist.Add(new Models.User { Username = "Ramya", Password = "ramya@123" });
+            userlist.Add(new Models.User { Username = "Ganesh", Password = "ganesh@123" });
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(User u)
+        {
+            var found = userlist.Find(f => ((f.Username == u.Username) && (f.Password == u.Password)));
+
+            if (found != null)
+            {
+                return RedirectToAction("Dashboard", "Home");
+            }
+
+            else
+            {
+                return View();
+            }
+        }
+
+
+        public IActionResult Dashboard()
+        {
+
+            return View();
+        }
+        //[HttpPost]
+        //public IActionResult Dashboard(FormCollection f)
+        //{
+
+        //    return RedirectToAction("Index");
+        //}
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
